41e2f44043c7950c2bb030509ea5bed1fee40fe8
subbaraju.challa <subbaraju.challa@lenskart.in>
2025-02-01 18:05:17 +0530

    update

---
+++ basics/myscript.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash  ## This searches for Bash in the system’s $PATH and runs the first matching version. It works even if Bash is installed in a non-standard location.
+echo "Hello, $USER! Welcome to Bash scripting."
